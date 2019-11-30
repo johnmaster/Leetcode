9d480be22726371f6d2b8f0d10a5d4b268064897
瞌睡 <30625198+johnmaster@users.noreply.github.com>
2019-11-30 16:49:16 +0800

    二叉搜索树的最近公共祖先

---
+++ 235.二叉搜索树的最近公共祖先/lowestCommonAncestor.py
@@ -0,0 +1,17 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        parent_val = root.val
+        p_val, q_val = p.val, q.val
+        if p_val > parent_val and q_val > parent_val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif p_val < parent_val and q_val < parent_val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return root
